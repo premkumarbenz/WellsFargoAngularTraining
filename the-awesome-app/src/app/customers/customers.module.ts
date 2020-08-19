0b9f7c78c243a5874998edd5036317a14d083a44
Anil Joseph <anil.jos@gmail.com>
2020-08-19 14:25:26 +0530

    Day 2 DB

--- the-awesome-app/src/app/customers/customers.module.ts
+++ the-awesome-app/src/app/customers/customers.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
 import { ListCustomersComponent } from './list-customers/list-customers.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [ListCustomersComponent],
   imports: [
-    CommonModule
+    CommonModule, HttpClientModule, FormsModule
   ],
   exports: [ListCustomersComponent]
 })
--- the-awesome-app/src/app/customers/list-customers/list-customers.component.ts
+++ the-awesome-app/src/app/customers/list-customers/list-customers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Customer} from '../model/customer'
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-list-customers',
@@ -9,15 +10,44 @@ import {Customer} from '../model/customer'
 export class ListCustomersComponent implements OnInit {
 
   data: Array<Customer>;
-  constructor() { 
+  customer: Customer = new Customer();
+  url: string;
+
+  constructor(private httpClient: HttpClient) { 
 
       this.data = new Array<Customer>();
-      this.data.push(new Customer(1, "Reliance", "Mumbai"));
-      this.data.push(new Customer(2, "Infosys", "Bangalore"));
-      this.data.push(new Customer(3, "Apple", "Hyderabad"));
+      this.url = "https://calm-beach-18228.herokuapp.com/customers";
+      // this.data.push(new Customer(1, "Reliance", "Mumbai"));
+      // this.data.push(new Customer(2, "Infosys", "Bangalore"));
+      // this.data.push(new Customer(3, "Apple", "Hyderabad"));
   }
 
   ngOnInit(): void {
+
+     ///var httpClient = new HttpClient();
+     //var url = "https://calm-beach-18228.herokuapp.com/customers";
+     this.httpClient
+              .get<Array<Customer>>(this.url)
+              .subscribe((data) => {
+                  console.log(data);
+                  this.data = data;
+              })
+
+
+  }
+  save(){
+
+    this.httpClient
+          .post(this.url, this.customer)
+          .subscribe(() => {
+              
+              alert("saved");
+              this.data.push(this.customer);
+              this.customer = new Customer();
+
+          }, () => {
+              alert("Failed");
+          })
   }
 
 }
