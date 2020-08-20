b89c965f5d5a1e3d61221cdfe4bde981b8fd91d0
Anil Joseph <anil.jos@gmail.com>
2020-08-20 11:14:14 +0530

    Day 3 Search

--- the-awesome-app/src/app/search/search.component.ts
+++ the-awesome-app/src/app/search/search.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {interval} from 'rxjs';
-import {take, tap, filter, map, debounceTime} from 'rxjs/operators';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { interval, Observable } from 'rxjs';
+import { take, tap, filter, map, debounceTime } from 'rxjs/operators';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-search',
@@ -11,18 +12,21 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class SearchComponent implements OnInit {
 
   searchFormGroup: FormGroup;
-  constructor() { 
+  results: Array<string>;
+  observableResults: Observable<Array<string>>;
 
-           let obs =  interval(1000);
-           obs = obs.pipe(
-              take(10),
-              tap(n => console.log("tap: ", n)),
-              filter(n => n % 2 == 0),
-              map(n => n * n)
-           );
-          //  obs.subscribe((result) => {
-          //     console.log("subscriber: ", result);
-          //  });
+  constructor(private http: HttpClient) {
+
+    let obs = interval(1000);
+    obs = obs.pipe(
+      take(10),
+      tap(n => console.log("tap: ", n)),
+      filter(n => n % 2 == 0),
+      map(n => n * n)
+    );
+    //  obs.subscribe((result) => {
+    //     console.log("subscriber: ", result);
+    //  });
   }
 
   ngOnInit(): void {
@@ -42,15 +46,57 @@ export class SearchComponent implements OnInit {
     //           console.log(value);
     //         })
 
-            search
-            .valueChanges
-            .pipe(
-              filter(v => search.valid),
-              debounceTime(1000)
-            )
-            .subscribe(value => {
-              console.log(value);
-            })
+    search
+      .valueChanges
+      .pipe(
+        debounceTime(1000),
+        filter(v => search.valid),
+
+      )
+      .subscribe(value => {
+        console.log(value);
+
+        const url = "https://en.wikipedia.org/w/api.php";
+
+        const httpParams = new HttpParams()
+          .set("action", "opensearch")
+          .set("limit", "20")
+          .set("origin", "*")
+          .set("search", value);
+
+        // this.http
+        //       .get(url, {params: httpParams})
+        //       .subscribe((data) => {
+        //         console.log(data)
+        //       });
+
+        // this.http
+        //   .get(url, { params: httpParams, observe: 'response' })
+        //   .subscribe((resp) => {
+        //     console.log(resp);
+        //     console.log(resp.body);
+        //     console.log(resp.status);
+        //     //console.log(resp.headers[]);
+        //   });
+
+        // this.http
+        //   .get(url, { params: httpParams, observe: 'body' })
+        //   .pipe(
+        //     map(data => data[1])
+        //   )
+        //   .subscribe((data) => {
+        //     console.log(data);
+        //     this.results = data;
+           
+        //   });
+
+        this.observableResults =  this.http
+                                  .get(url, { params: httpParams, observe: 'body' })
+                                  .pipe(
+                                    map(data => data[1])
+                                  );
+         
+      })
   }
 
 }
